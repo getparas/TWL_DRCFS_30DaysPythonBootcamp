0eeeca5821fb781b0089ef7350a6abd3edeafbb5
pritha <prithakarki030@gmail.com>
2022-12-12 19:58:13 +0545

    add function code

---
+++ Week 2/personal/functions.py
@@ -0,0 +1,17 @@
+# A function is a block of code that performs a specific task.
+def add_one(num):
+    result = num + 1
+    print(result)
+    
+add_one(5)
+
+def greet(name):
+    print("Hello",name,"!")
+greet("iCoder")
+
+def square(number):
+    result = number * number
+    return result
+
+x = square(5)
+print(x)
